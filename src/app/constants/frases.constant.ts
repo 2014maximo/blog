8b5f8e827f0d8057ea2c75112313f69b61241b9f
2014maximo <alexuriel.777@gmail.com>
2021-08-18 22:59:51 -0500

    sincronizando cambios

--- src/app/constants/frases.constant.ts
+++ src/app/constants/frases.constant.ts
@@ -4,49 +4,168 @@ export const FRASES = [
         autor: 'Pedro Bergman',
         borde: 'border-secondary',
         colorText: 'text-secondary',
-        id:'1'
+        id:1
     },
     {
         frase: 'La persona que pregunta es tonta por un minuto. La persona que se queda con la duda permanece tonta por siempre',
         autor: 'Albert Einstein',
         borde: 'border-primary',
         colorText: 'text-primary',
-        id:'2'
+        id:2
     },
     {
         frase: 'Lo mismo que el hierro se oxida por falta de uso y el agua estancada se vuelve putrefacta, también la inactividad destruye el intelecto.',
         autor: 'Leonardo Da Vinci',
         borde: 'border-secondary',
         colorText: 'text-secondary',
-        id:'3'
+        id:3
     },
     {
         frase: 'Quien quiere hacer algo, encuentra los medios, quien no quiere, encuentra miles de escusas.',
         autor: 'Proverbio Árabe',
         borde: 'border-success',
         colorText: 'text-success',
-        id:'4'
+        id:4
     },
     {
         frase: 'La lógica te llevará de la A-Z, pero la imaginación te llevará a todas partes.',
         autor: 'Albert Einstein',
         borde: 'border-danger',
         colorText: 'text-danger',
-        id:'5'
+        id:5
     },
     {
         frase: 'Esperar que la vida te trate bien porque eres buena persona, es como esperar que un tigre no te ataque porque eres vegetariano.',
         autor: 'Bruce Lee',
-        borde: 'border-danger',
+        borde: 'border-warning',
         colorText: 'text-warning',
-        id:'6'
+        id:6
     },
     {
         frase: 'Cuida tus pensamientos, porque se convertirán en tus palabras. Cuida tus palabras, porque se convertirán en tus actos. Cuida tus actos, porque se convertirán en tus hábitos. cuida tus hábitos, porque se convertirán en tu destino.',
         autor: 'Mahatma Gandhi',
-        borde: 'text-info',
+        borde: 'border-info',
         colorText: 'text-info',
-        id:'7'
+        id:7
+    },
+    {
+        frase: 'Somos arquitectos de nuestro propio destino.',
+        autor: 'Albert Einstein',
+        borde: 'border-secondary',
+        colorText: 'text-secondary',
+        id:8
+    },
+    {
+        frase: 'Intenta no volverte un hombre de éxito, sino un hombre de valor.',
+        autor: 'Albert Einstein',
+        borde: 'border-primary',
+        colorText: 'text-primary',
+        id:9
+    },
+    {
+        frase: 'Entre las dificultades se esconde la oportunidad.',
+        autor: 'Albert Einstein',
+        borde: 'border-secondary',
+        colorText: 'text-secondary',
+        id:10
+    },
+    {
+        frase: 'No necesito saberlo todo, tan sólo necesito saber donde encontrar aquello que me hace falta, cuando lo necesite.',
+        autor: 'Albert Einstein',
+        borde: 'border-success',
+        colorText: 'text-success',
+        id:11
+    },
+    {
+        frase: 'La mente que se abre a una nueva idea jamás vuelve a su tamaño original.',
+        autor: 'Albert Einstein',
+        borde: 'border-danger',
+        colorText: 'text-danger',
+        id:12
+    },
+    {
+        frase: 'Siento una enorme gratitud por todos los que me dijeron no, gracias a ellos lo hice yo mismo.',
+        autor: 'Albert Einstein',
+        borde: 'border-warning',
+        colorText: 'text-warning',
+        id:13
+    },
+    {
+        frase: 'Vivimos en un mundo en el que la verdad es opacada por una realidad que no existe.',
+        autor: 'Albert Einstein',
+        borde: 'border-info',
+        colorText: 'text-info',
+        id:14
+    },
+    {
+        frase: 'El inteligente no es aquel que lo sabe todo, sino aquel que sabe utilizar lo poco que sabe.',
+        autor: 'Sebastian Cohen Saavedra',
+        borde: 'border-secondary',
+        colorText: 'text-secondary',
+        id:15
+    },
+    {
+        frase: 'Si no eres agradecido con lo que tienes, ¿que te hace pensar que serás feliz si tuvieras más?',
+        autor: 'Anónimo',
+        borde: 'border-primary',
+        colorText: 'text-primary',
+        id:16
+    },
+    {
+        frase: 'Gastamos dinero que no tenemos, en cosas que no necesitamos, para impresionar a gente a la que no le importamos.',
+        autor: 'Will Smith',
+        borde: 'border-secondary',
+        colorText: 'text-secondary',
+        id:17
+    },
+    {
+        frase: 'Que tu único vicío sea seguir aprendiendo.',
+        autor: 'Anónimo',
+        borde: 'border-success',
+        colorText: 'text-success',
+        id:18
+    },
+    {
+        frase: 'Tienes que aprender la reglas del juego y despues jugar mejor que nadie.',
+        autor: 'Albert Einstein',
+        borde: 'border-danger',
+        colorText: 'text-danger',
+        id:19
+    },
+    {
+        frase: 'Tienes que aprender la reglas del juego y despues jugar mejor que nadie.El diablo susurró en mi oído, -No eres lo suficientemente fuerte para resistir la tormenta. Hoy le susurré al diablo en el oído, -Yo soy la tormenta.',
+        autor: 'Anónimo',
+        borde: 'border-warning',
+        colorText: 'text-warning',
+        id:20
+    },
+    {
+        frase: 'Todo lo que quieres en la vida está fuera de tu zona de confort.',
+        autor: 'Robert Allen',
+        borde: 'border-info',
+        colorText: 'text-info',
+        id:21
+    },
+    {
+        frase: 'Dios no creo el mal. El mal es el resultado de ausencia de Dios en el corazón de los hombres.',
+        autor: 'Albert Einstein',
+        borde: 'border-secondary',
+        colorText: 'text-secondary',
+        id:22
+    },
+    {
+        frase: 'El más grande obstáculo que le impide a la gente alcanzar el éxito, es tener en su mente una fábrica de excusas.',
+        autor: 'Mauricio Duarte',
+        borde: 'border-success',
+        colorText: 'text-success',
+        id:23
+    },
+    {
+        frase: 'La fuerza no viene de la capacidad corporal, sino de la voluntad del alma.',
+        autor: 'Gandhi',
+        borde: 'border-danger',
+        colorText: 'text-danger',
+        id:24
     },
 
 ]
--- src/app/constants/sliders.constant.ts
+++ src/app/constants/sliders.constant.ts
@@ -1,27 +1,27 @@
 export const RUTAS_SLIDES = [
         {
-            ruta: '../../../assets/img/sliders/home-desktop/dart.jpg',
+            ruta: 'assets/img/sliders/home-desktop/dart.jpg',
             alt: 'Dart',
             height: '600',
             width: 'auto',
             id:'1'
         },
         {
-            ruta: '../../../assets/img/sliders/home-desktop/android.jpg',
+            ruta: 'assets/img/sliders/home-desktop/android.jpg',
             alt: 'Android',
             height: '600',
             width: 'auto',
             id:'2'
         },
         {
-            ruta: '../../../assets/img/sliders/home-desktop/angular.jpg',
+            ruta: 'assets/img/sliders/home-desktop/angular.jpg',
             alt: 'Angular',
             height: '600',
             width: 'auto',
             id:'3'
         },
         {
-            ruta: '../../../assets/img/sliders/home-desktop/flutter.jpg',
+            ruta: 'assets/img/sliders/home-desktop/flutter.jpg',
             alt: 'Flutter',
             height: '600',
             width: 'auto',
