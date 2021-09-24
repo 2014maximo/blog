b4e1b5c40146efb009127d78e5e6096526718950
2014maximo <alexuriel.777@gmail.com>
2021-09-24 01:27:52 -0500

    accordeon y post

---
+++ src/app/shared/accordeon/accordeon.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AccordeonPostModel } from '../../models/post.model';
+
+@Component({
+  selector: 'app-accordeon',
+  templateUrl: './accordeon.component.html',
+  styleUrls: ['./accordeon.component.css']
+})
+export class AccordeonComponent implements OnInit {
+
+  @Input() contenido: AccordeonPostModel = {
+    idHeading: '',
+
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
