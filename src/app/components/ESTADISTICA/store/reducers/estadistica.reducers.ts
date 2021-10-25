a4f2fc8b92b6219b156e58a2193e168d584951cc
2014maximo <alexuriel.777@gmail.com>
2021-10-24 21:01:44 -0500

    refector a anterior

--- src/app/components/ESTADISTICA/store/reducers/estadistica.reducers.ts
+++ src/app/components/ESTADISTICA/store/reducers/estadistica.reducers.ts
@@ -1,11 +1,25 @@
 import { createReducer, on } from '@ngrx/store';
 import { cargarSorteos, cargarSorteosError, cargarSorteosSuccess } from '../actions';
 
-export interface State {
-    key: String;
+export interface SorteoState {
+    sorteos : [],
+    loaded  : boolean,
+    loading : boolean,
+    error   : any
 }
 
-export const initialState: State = {
-    key: 'hola'
+export const sorteosInitialState: SorteoState = {
+    sorteos : [],
+    loaded  : false,
+    loading : false,
+    error   : null
 }
+
+const _counterReducer = createReducer( sorteosInitialState, 
+    on( cargarSorteos, state => ({ ...state, loading: true })),
+);
+
+/* export function counterReducer(state, action) {
+    return _counterReducer(state, action);
+} */
 
