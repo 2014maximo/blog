d2135fae2cd130fd9ddcd539b2a2b0579cfc8ce4
almunoz <almunoz@cotrafa.com.co>
2022-05-23 14:06:41 -0500

    add component code

---
+++ src/app/shared/print-code/print-code.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-print-code',
+  templateUrl: './print-code.component.html',
+  styleUrls: ['./print-code.component.css']
+})
+export class PrintCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
