c46817edc761c0f6336d338df607b9c9b387e935
2014maximo <alexuriel.777@gmail.com>
2023-03-23 01:22:23 -0500

    content glosario angular

--- src/app/constants/glosarios/terminos.angular.constant.ts
+++ src/app/constants/glosarios/terminos.angular.constant.ts
@@ -5,37 +5,28 @@ export const TERMINOS_ANGULAR: GlosarioModel [] = [
     {
         id: 'bfbfe921-9666-402f-98d3-98f45716b57f',
         titulo: 'firstValueFrom (RXJS)',
-        linkReferencia: '',
+        linkReferencia: 'ng-rxjs-1',
         descripcion: [
-            'Aparece apartir de la versión 7, viene a reemplazar al "toPromise(), quedando enterrado, obsoleto."',
-            'Su especialidad es devolver el primer valor del observable y algo muy importante es que nos convierte el observable en promesa, o sea que le asigna la propiedad de la sincronicidad.'
+            'No nos va a aparecer antes de la versión 7 de la libreria rxjs, este viene a reemplazar la "toPromise()", quedando en el abismo de los obsoletos.',
+            'Su característica de operar es la de convertir un observable en una promesa que se cumple cuando devuelve el primer valor.'
         ]
     },
     {
-        id: 'bfbfe921-9666-402f-98d3-98f45716b57f',
-        titulo: 'map (RXJS)',
-        linkReferencia: '',
+        id: '6f3b8325-3262-421f-ac5a-7ed4946487a6',
+        titulo: 'lastValueFrom() (RXJS)',
+        linkReferencia: 'ng-rxjs-1',
         descripcion: [
-            'Aparece apartir de la versión 7, viene a reemplazar al "toPromise(), quedando enterrado, obsoleto."',
-            'Su especialidad es devolver el primer valor del observable y algo muy importante es que nos convierte el observable en promesa, o sea que le asigna la propiedad de la sincronicidad.'
+            'No nos va a aparecer antes de la versión 7 de la libreria rxjs, este viene a reemplazar la "toPromise()", quedando en el abismo de los obsoletos.',
+            'Su característica de operar es la de convertir un observable en una promesa que se cumple cuando devuelve el último valor.'
         ]
     },
     {
-        id: 'bfbfe921-9666-402f-98d3-98f45716b57f',
-        titulo: 'from (RXJS)',
-        linkReferencia: '',
+        id: 'ebfeb1a0-10d5-49b0-aaa2-4196e8f1dd98',
+        titulo: 'first (RXJS)',
+        linkReferencia: 'ng-rxjs-1',
         descripcion: [
-            'Aparece apartir de la versión 7, viene a reemplazar al "toPromise(), quedando enterrado, obsoleto."',
-            'Su especialidad es devolver el primer valor del observable y algo muy importante es que nos convierte el observable en promesa, o sea que le asigna la propiedad de la sincronicidad.'
+            'En simples palabras devuelve el primer elemento emitido y termina una vez lo reciba.'
         ]
     },
-    {
-        id: 'bfbfe921-9666-402f-98d3-98f45716b57f',
-        titulo: 'fromEvent (RXJS)',
-        linkReferencia: '',
-        descripcion: [
-            'Aparece apartir de la versión 7, viene a reemplazar al "toPromise(), quedando enterrado, obsoleto."',
-            'Su especialidad es devolver el primer valor del observable y algo muy importante es que nos convierte el observable en promesa, o sea que le asigna la propiedad de la sincronicidad.'
-        ]
-    },
+
 ]
