f5ef1431a186e8adfcd0ffd364a2c6a76b4066fe
2014maximo <alexuriel.777@gmail.com>
2023-09-04 01:18:00 -0500

    avance en logica de reemplazo en print-code

--- src/app/shared/components/print-code/constants/referenciasTypescript.constant.ts
+++ src/app/shared/components/print-code/constants/referenciasTypescript.constant.ts
@@ -6,17 +6,34 @@ export const REFERJAVASCRIPT = [
     {
         referencia: 'function',
         retorno:'<span class="c3">function</span>'
+    },
+    {
+        referencia: '{',
+        retorno: '<span class="text-light">{</span>'
+    },
+    {
+        referencia: '}',
+        retorno: '<span class="text-light">}</span>'
     }
 ]
 
 export function cargarTypescript(plantilla:string):string{
 
+    // La idea es iterar cada referencia
+    // Extraer todo lo que debe traer hasta su final propio
+    // Se ubica la p
+    // Reemplazarlo por los estilos propuestos
+    // 
+
     let formado: string = plantilla;
-    REFERJAVASCRIPT.forEach((e:any, i:number)=>{
-        console.log(formado);
-        formado = plantilla.replaceAll(e.referencia, e.retorno);
-        console.log(formado)
-    });
+    
+    for(let i = 0; i < REFERJAVASCRIPT.length; i++){
+        let ubicar = REFERJAVASCRIPT[i].referencia;
+        let posicion = plantilla.indexOf(ubicar);
+        console.log(posicion);
+        // formado = formado.replace(REFERJAVASCRIPT[i].referencia, traerRefencia(REFERJAVASCRIPT[i].referencia));
+    }
+
     let contadorSaltos: number = 0;
 
     formado = '<pre class="fuenteTres fw-5 fs-17">'.concat(formado);
@@ -26,4 +43,18 @@ export function cargarTypescript(plantilla:string):string{
 
 export function traerRefencia(actual:string): string{
     return REFERJAVASCRIPT.find( e => e.referencia === actual)?.retorno ?? actual;
+}
+
+export function devolverUbicacion(cadena: string, referencia: string):number{
+    
+/*     for (let i = 0; i < cadena.length; i++){
+        if(cadena[i] === )
+    } */
+
+
+    return 0;
+}
+
+class caminar {
+    
 }
