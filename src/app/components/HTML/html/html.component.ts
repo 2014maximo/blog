b7f661e5cdf3ed085920b4e5702c4bd656f33206
2014maximo <alexuriel.777@gmail.com>
2023-09-14 12:28:52 -0500

    se agrega categoria html

--- src/app/components/HTML/html/html.component.ts
+++ src/app/components/HTML/html/html.component.ts
@@ -9,57 +9,14 @@ import { IndiceDeContenidosModel } from '../../../shared/models/indice.model';
 })
 export class HtmlComponent implements OnInit {
 
-  public cabeceraPost: HeaderPostModel = {
-    rutaImagen: '',
-    alturaImagen: '',
-    fondo: false,
-    tituloPost: '',
-    sombra: ''
-  }
-
-  public indiceHtml: IndiceDeContenidosModel [] = [];
-  public claseHtml = '';
+  public idPost = '8825362007';
 
   constructor() {
-    this.inicializarVariables();
   }
 
   ngOnInit(): void {
   }
-
-  private inicializarVariables() {
-    this.cabeceraPost = {
-      rutaImagen: 'assets/img/categorias/html-white.png',
-      fondo: true,
-      tituloPost: '',
-      alturaImagen: '150',
-      sombra: 'drop'
-    };
-
-    this.claseHtml = 'mt-2';
-
-    this.indiceHtml = [
-      {
-        nombre: 'ETIQUETAS',
-        posicion: 'pos-z',
-        ruta: 'html-etiquetas',
-        colorFondo: 'bg-Html',
-        color: 'text-light',
-        estado: 'activo'
-      },
-      {
-        nombre: 'PROPIEDADES',
-        posicion: 'pos-z',
-        ruta: 'html-propiedades',
-        colorFondo: 'bg-Html',
-        color: 'text-light',
-        estado: 'inactivo'
-      }
-    ]
-  }
-
   scroll(el: HTMLElement) {
     el.scrollIntoView();
   }
-
 }
--- src/app/constants/publicaciones/posts-html.constant.ts
+++ src/app/constants/publicaciones/posts-html.constant.ts
@@ -24,5 +24,33 @@ export const HTML: DatosPost[] = [
         imgCuadro:'',
         posicion: 'rot-1',
         estado: 'inactivo'
+    },
+    {
+        id: '2b9d0c67-2262-4f96-8179-483d21f71967',
+        nombre: 'Elementos HTML',
+        descripcion: [
+            'Descripción rápida de la aplicación de lo elementos que hacen parte de typescript, similares en gran parte a Javascript, ya obviamente la aplicación de lo particular de typescript que son los tipos.'
+        ],
+        descripcionCorta: 'Detalle de los elementos que hacen parte de Typescript, detallando en contraparte a Javascript.',
+        ruta: 'html-etiquetas',
+        componente: '',
+        mostrarEnPostHome: true,
+        estilos: ESTILO_HTML,
+        fechaCreacion: '2023-09-14',
+        fechaActualizacion: '',
+        imgHorizontal:'https://plantillas_dev.gitlab.io/assets/img/posts/angular/instalacion-angular-horizontal.jpg',
+        imgVertical:'https://plantillas_dev.gitlab.io/assets/img/posts/angular/instalacion-angular-cuadro.jpg',
+        imgCuadro:'https://plantillas_dev.gitlab.io/assets/img/posts/angular/instalacion-angular-cuadro.jpg',
+        categoria: 'html',
+        posicion: 'rot-2',
+        estado: 'activo',
+        imgSlider: {
+            alt: 'Instalación Angular y recomendados',
+            height: '600',
+            id: '2',
+            ruta: 'https://plantillas_dev.gitlab.io/assets/img/posts/angular/instalacion-angular-SLIDER.jpg',
+            width: 'auto',
+            post: 'ng-instalacion'
+        }
     }
 ]
