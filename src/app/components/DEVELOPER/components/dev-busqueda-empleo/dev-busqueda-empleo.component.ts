1b59a966ad672642fcb802606f4a6764d34ac695
axuriel <auriel@smartsoftlabs.com>
2023-11-14 23:00:34 -0500

    organizando primera traducion de post completa

--- src/app/components/DEVELOPER/components/dev-busqueda-empleo/dev-busqueda-empleo.component.ts
+++ src/app/components/DEVELOPER/components/dev-busqueda-empleo/dev-busqueda-empleo.component.ts
@@ -11,63 +11,64 @@ import { IAgencia } from './models/empleo.model';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-dev-busqueda-empleo',
-  templateUrl: './dev-busqueda-empleo.component.html',
-  styleUrls: ['./dev-busqueda-empleo.component.css']
+	selector: 'app-dev-busqueda-empleo',
+	templateUrl: './dev-busqueda-empleo.component.html',
+	styleUrls: ['./dev-busqueda-empleo.component.css']
 })
 export class DevBusquedaEmpleoComponent implements OnInit {
 
-  public idPublicacion = '27df71a4-6654-4349-9481-06f93c3639d6';
-  public indiceTypescript: IndiceDeContenidosModel [] = [];
-  public publicacion = new DatosPost();
-  public categoria = new CategoriaPostModel();
-  public breadcrumb = new CategoriaModel();
-  public marketing: string[] = [];
-  public preguntas:PreguntasModel[]=[];
-  public agencias: IAgencia[]=[];
-  public bloqueLinks: IndiceDeContenidosModel[]=[];
-  public text = TEST;
-  public textosATraducir: string[] = ['ejer', 'ejer1', 'ejer2'];
-  public textosTraducidos: string[] = [];
+	public idPublicacion = '27df71a4-6654-4349-9481-06f93c3639d6';
+	public indiceTypescript: IndiceDeContenidosModel[] = [];
+	public publicacion = new DatosPost();
+	public categoria = new CategoriaPostModel();
+	public breadcrumb = new CategoriaModel();
+	public marketing: string[] = [];
+	public preguntas: PreguntasModel[] = [];
+	public agencias: IAgencia[] = [];
+	public bloqueLinks: IndiceDeContenidosModel[] = [];
+	public text = TEST;
+	public textosATraducir: any;
+	public textosTraducidos: string[] = [];
 
 
-  constructor(public translate: TranslateService) {
-    translate.setDefaultLang(navigator.language.split('-')[0]);
-  }
+	constructor(public translate: TranslateService) {
+		translate.setDefaultLang(navigator.language.split('-')[0]);
+	}
 
-  ngOnInit(): void {
-    this.traducirTextos();
-    this.publicacion = postActual(this.idPublicacion)[0];
-    this.categoria = CATEGORIA.filter( e => e.nombre === this.publicacion.categoria)[0];
-    this.indiceTypescript = cargarIndice(this.publicacion);
-    this.breadcrumb = cargarBreadcrumb(this.publicacion);
-    this.agencias = AGENCIAS;
-    this.marketing = MARKETING;
-    this.preguntas = PREGUNTAS;
-    this.bloqueLinks = BLOQUE_LINKS.sort((a:any,b:any) => a.nombre.toLowerCase().charCodeAt(0) - b.nombre.toLowerCase().charCodeAt(0));
-  }
+	ngOnInit(): void {
+		this.textosTraducidos = ['ejer','ejer1','ejer2']
+		this.traducirTextos();
+		this.publicacion = postActual(this.idPublicacion)[0];
+		this.categoria = CATEGORIA.filter(e => e.nombre === this.publicacion.categoria)[0];
+		this.indiceTypescript = cargarIndice(this.publicacion);
+		this.breadcrumb = cargarBreadcrumb(this.publicacion);
+		this.agencias = AGENCIAS;
+		this.marketing = MARKETING;
+		this.preguntas = PREGUNTAS;
+		this.bloqueLinks = BLOQUE_LINKS.sort((a: any, b: any) => a.nombre.toLowerCase().charCodeAt(0) - b.nombre.toLowerCase().charCodeAt(0));
+	}
 
-  public scroll(el: HTMLElement){
-    el.scrollIntoView();
-  }
+	public scroll(el: HTMLElement) {
+		el.scrollIntoView();
+	}
 
-  public idFormat(refer:string, id?:number):string{
-      return id? `${refer.toLowerCase()}+${id}` : `${refer.toLowerCase()}`;
-  }
+	public idFormat(refer: string, id?: number): string {
+		return id ? `${refer.toLowerCase()}+${id}` : `${refer.toLowerCase()}`;
+	}
 
-  public activo(e:any):string{
-    return e.srcElement.ariaExpanded?'active' : '';
-  }
+	public activo(e: any): string {
+		return e.srcElement.ariaExpanded ? 'active' : '';
+	}
 
-  public cambiarLenguaje(lang: string) {
-    this.translate.use(lang); // Cambia el actual lenguaje
-  }
+	public cambiarLenguaje(lang: string) {
+		this.translate.use(lang); // Cambia el actual lenguaje
+		this.traducirTextos();
+	}
 
-  traducirTextos(){
-    this.textosATraducir.forEach(key => {
-      const traduccion = this.translate.instant(key);
-      this.textosTraducidos.push(traduccion);
-    });
-  }
+	traducirTextos() {
+		this.translate.get('DEV.be').subscribe((translated: string) => {
+			this.textosATraducir = translated;
+		});
+	}
 
 }
--- src/app/shared/components/encabezado-publicacion/encabezado-publicacion.component.ts
+++ src/app/shared/components/encabezado-publicacion/encabezado-publicacion.component.ts
@@ -1,43 +1,44 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { datosCategoria, postActual, textoAFecha } from '../../constants/funciones/funciones-globales';
 import { DatosPost } from '../../models/categorias.model';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-encabezado-publicacion',
-  templateUrl: './encabezado-publicacion.component.html',
-  styleUrls: ['./encabezado-publicacion.component.css']
+	selector: 'app-encabezado-publicacion',
+	templateUrl: './encabezado-publicacion.component.html',
+	styleUrls: ['./encabezado-publicacion.component.css']
 })
 export class EncabezadoPublicacionComponent implements OnInit {
 
-  @Input() idPublicacion: string = '';
-  @Input() rutaIconoSugerido: string = '';
+	@Input() idPublicacion: string = '';
+	@Input() rutaIconoSugerido: string = '';
 
-  public publicacion = new DatosPost();
+	public publicacion = new DatosPost();
 
-  constructor() { }
+	constructor(public translate: TranslateService) {
+		translate.setDefaultLang(navigator.language.split('-')[0]);
+	}
 
-  ngOnInit(): void {
-    this.inicializarVariables();
-  }
-  ngOnDestroy(): void {
-    
-    
-  }
+	ngOnInit(): void {
+		this.inicializarVariables();
+	}
+	ngOnDestroy(): void {
+	}
 
-  public extraerDatoCategoria(categoria: string, referencia: number):any{
-    return datosCategoria(categoria, referencia)
-  }
+	public extraerDatoCategoria(categoria: string, referencia: number): any {
+		return datosCategoria(categoria, referencia)
+	}
 
-  private inicializarVariables(){
-    this.publicacion = (postActual(this.idPublicacion))[0];
-  }
+	private inicializarVariables() {
+		this.publicacion = (postActual(this.idPublicacion))[0];
+	}
 
-  public fechaAString(fecha: Date): string{
-    return new Date(fecha).toISOString().slice(0,10);
-  }
+	public fechaAString(fecha: Date): string {
+		return new Date(fecha).toISOString().slice(0, 10);
+	}
 
-  public textoAFecha(fecha: string): Date{
-    return textoAFecha(fecha);
-  }
+	public textoAFecha(fecha: string): Date {
+		return textoAFecha(fecha);
+	}
 
 }
